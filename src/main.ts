f1a59c447ac1a0da0effb5d910994d51ceedd957
Ilesandres <ilesandres6@gmil.com>
2025-04-13 19:12:35 -0500

    entorno

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { SERVER_PORT } from './config/constants';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule,{cors:true});
+  const configService= app.get(ConfigService);
+  const port= configService.get(SERVER_PORT)
+  await app.listen(port?? 3000);
+}
+bootstrap();
